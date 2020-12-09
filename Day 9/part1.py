41d254859b8e341a3643186eafa97dcfad2fa8b4
jordankehrer <jordankehrer@mac.com>
2020-12-09 00:22:31 -0500

    Day 9 part 1

---
+++ Day 9/part1.py
@@ -0,0 +1,21 @@
+import os, pathlib
+
+inputfile = os.path.join(pathlib.Path(__file__).parent, "input.txt")
+inputs = open(inputfile, 'r').read().splitlines()
+
+def get_possible_sums(values):
+    possibilities = set()
+    while len(values) > 0:
+        value_to_consider = values.pop()
+        possibilities = possibilities.union(set([value_to_consider + x for x in values if x is not value_to_consider]))
+    return possibilities
+
+numbers = [int(x) for x in inputs]
+
+for i in range(25, len(numbers)):
+    possibilities = get_possible_sums(numbers[i-25:i])
+    if numbers[i] not in possibilities:
+        break
+
+
+print(numbers[i])
