a540296cf67e4f5407bfbdb61aecf07c670e8b8e
jordankehrer <jordankehrer@mac.com>
2020-12-19 20:18:36 -0500

    day 18 part 2

---
+++ Day 18/part2.py
@@ -0,0 +1,45 @@
+import os, pathlib
+import re 
+
+inputfile = os.path.join(pathlib.Path(__file__).parent, "input.txt")
+inputs = open(inputfile, 'r').read().splitlines()
+
+
+def add(a,b):
+    return a + b
+
+def mul(a,b):
+    return a * b
+
+def replace(matcher):
+    string = matcher.group(1)
+    return str(do_math(string))
+    
+def do_math(string):
+    if "*" in string and "+" in string:
+        return process(re.sub('\d* \+ \d*', '(\g<0>)', string, 1))
+
+    exp = string.split(' ')
+    left = exp.pop(0)
+    while len(exp) > 0:
+        right = exp.pop(0)
+        if right == "+":
+            op = add
+        elif right == "*":
+            op = mul
+        else:
+            left = op(int(left), int(right))
+    return left
+
+
+def process(exp):
+    if '(' in exp:
+        return process(re.sub('\(([^()]*)\)', replace, exp))
+    return do_math(exp)
+
+
+answers = []
+for exp in inputs:
+    answers.append(process(exp))
+
+print(sum(answers))
